6b7fc220bf40b53eac307995efaa2dcf316e5fe6
Nakazen - san <naufalkaruniawanzain@gmail.com>
2024-11-08 19:35:10 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<x-main-layout title="Home Page">
+  <h1>Selamat Datang di Halaman Utama</h1>
+  
+</x-main-layout>
---
+++ resources/views/layouts/nav-user.blade.php
@@ -0,0 +1,110 @@
+<div class="navbar bg-base-100 h-[90px] border-b-2">
+  <div>
+    <a class="ml-4"><img src="img/logo-bms.png" width="70px" height="auto"></a>
+    <div class="ml-3">
+    <p class="text-lg font-black">Desa Karangklesem Kecamatan Pekuncen</p>
+    <p class="text-lg font-black text-red-500 -mt-2 -mb-1">Pemerintah Kabupaten Banyumas</p>
+    <p class=" text-xs font-bold">Jl. PRAMUKA NO 645 KARANGKLESEM KEC.PEKUNCEN KODE POS 53164 TELP. 0281 572192</p>
+  </div>
+  </div>
+</div>
+<div class="navbar bg-gray-900 h-[30px] border-b-2">
+  <div class="navbar-start text-white">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center hidden lg:flex">
+    <ul class="menu text-white menu-horizontal px-1">
+      <li><a>Beranda</a></li>
+      <li>
+        <details>
+          <summary>Profil</summary>
+          <ul class="p-2 text-black">
+            <li><a>Visi Misi</a></li>
+            <li><a>Sejarah Desa</a></li>
+            <li><a>Wilayah Desa</a></li>
+            <li><a>Tokoh sejarah</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary>Lembaga Desa</summary>
+          <ul class="p-2 text-black">
+            <li><a>Pemerintah Desa</a></li>
+            <li><a>Badan Permusyawaratan Desa</a></li>
+            <li><a>Daftar RT/RW</a></li>
+            <li><a>Tokoh sejarah</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Peraturan Desa</a></li>
+      <li>
+        <details>
+          <summary>Potensi Desa</summary>
+          <ul class="p-2 text-black">
+            <li><a>Potensi Unguulan</a></li>
+            <li><a>Potensi Wisata</a></li>
+            <li><a>Produk Desa</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary>Menu Publik</summary>
+          <ul class="p-2 text-black">
+            <li><a>Berita</a></li>
+            <li><a>Informasi</a></li>
+            <li><a>Pengumuman</a></li>
+            <li><a>Undangan</a></li>
+            <li><a>Buku Tamu</a></li>
+            <li><a>Pengaduan</a></li>
+            <li><a>Agenda Kegiatan</a></li>
+            <li><a>Hubungi kami</a></li>
+            <li><a>Galeri</a></li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <details>
+          <summary>Pelayanan</summary>
+          <ul class="p-2 text-black">
+            <li><a>Pernikahan</a></li>
+            <li><a>Akte Kelahiran</a></li>
+            <li><a>Lain Lain</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+  </div>
+  <div class="navbar-end">
+    
+  </div>
+</div>
